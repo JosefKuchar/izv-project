f188d75c36106e5a0ea2207b06ce7100ed7fe8aa
Josef Kuchař <josef.kuchar267@gmail.com>
2023-11-14 15:15:11 +0100

    Part 2 done

--- part02/analysis.py
+++ part02/analysis.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3.11
 # coding=utf-8
+"""
+Analysis of data from Czech police
+
+# Author: Josef Kuchař (xkucha28)
+"""
 
 import io
 import zipfile
@@ -22,7 +27,7 @@ def load_data(filename: str) -> pd.DataFrame:
                "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t",
                "p5a"]
 
-    # Region codes (switched keys and values for easier user)
+    # Region codes (switched keys and values for easier use)
     regions = {
         "00": "PHA",
         "01": "STC",
@@ -115,8 +120,35 @@ def plot_state(df: pd.DataFrame, fig_location: str = None,
     :param show_figure: whether to show figure
     """
 
-    #TODO
-    sns.countplot(x="region", hue="p53", data=df)
+    # State of driver captions
+    p57 = {
+        4: "pod vlivem alkoholu, obsah alkoholu v krvi do 0,99 ‰",
+        5: "pod vlivem alkoholu, obsah alkoholu v krvi 1 ‰ a více",
+        6: "nemoc, úraz apod.",
+        7: "invalida",
+        8: "řidič při jízdě zemřel (infarkt apod.)",
+        9: "pokus o sebevraždu, sebevražda",
+    }
+
+    # Set style
+    sns.set_style("darkgrid")
+    # Filter data and replace state of driver codes with captions
+    data = df.query("p57 in [4,5,6,7,8,9]").replace({"p57": p57})
+    # Groupby and aggregate
+    data = data.groupby(["region", "p57"])
+    data = data.agg({"p1": "count"}).sort_values(["region"]).reset_index()
+    # Plot data
+    g = sns.catplot(data=data, x="region", y="p1", col="p57", kind="bar", hue="region",
+                    col_wrap=2, sharey=False, legend=False, aspect=1.1)
+    # Set labels
+    g.set_titles("Stav: {col_name}")
+    g.set_axis_labels("Kraj", "Počet nehod")
+    # Set title for whole figure
+    plt.subplots_adjust(top=0.95)
+    g.fig.suptitle("Počet nehod dle stavu řidiče při nedobrém stavu")
+    # Set custom face color
+    for ax in g.axes:
+        ax.set_facecolor("lightgrey")
 
     if fig_location is not None:
         plt.savefig(fig_location)
@@ -133,8 +165,25 @@ def plot_alcohol(df: pd.DataFrame, fig_location: str = None,
     :param show_figure: whether to show figure
     """
 
-    #TODO
-    sns.countplot(x="region", hue="p53", data=df)
+    # Query data
+    data = df.query("region in ['JHM','MSK','OLK','ZLK']").copy()
+    # Get hours from time
+    data["p2b"] = data["p2b"] // 100
+    data["alcohol"] = (data["p11"] >= 3).replace({True: "Ano", False: "Ne"})
+    # Filter out hours that are not in range 0-23
+    data = data.query("p2b >= 0 and p2b <= 23")
+    # Groupby and aggregate
+    data = data.groupby(["region", "p2b", "alcohol"]).agg({"p1": "count"}).reset_index()
+    # Plot data
+    g = sns.catplot(data=data, y="p1", x="p2b", col="region", hue="alcohol",
+                    hue_order=["Ano", "Ne"], kind="bar", col_wrap=2, sharex=False, sharey=False)
+    # Set ylim
+    g.set(ylim=(0, 4200))
+    # Set labels
+    g.set_titles("Kraj: {col_name}")
+    g.set_axis_labels("Hodina", "Počet nehod")
+    # Set legend title
+    g.legend.set_title("Alkohol")
 
     if fig_location is not None:
         plt.savefig(fig_location)
@@ -151,8 +200,40 @@ def plot_fault(df: pd.DataFrame, fig_location: str = None,
     :param show_figure: whether to show figure
     """
 
-    #TODO
-    sns.countplot(x="region", hue="p53", data=df)
+    p10 = {
+        1: "řidičem motorového vozidla",
+        2: "řidičem nemotorového vozidla",
+        3: "chodcem",
+        4: "lesní zvěří, domácím zvířectvem"
+    }
+
+    # Query data
+    data = df.query("region in ['JHM','MSK','OLK','ZLK']").copy()
+    data = data.query("p10 in [1,2,3,4]").replace({"p10": p10})
+    # Pivot data
+    data = data.pivot_table(index=["region", "p2a"], columns="p10", aggfunc="size", fill_value=0)
+    # Unstack data
+    data = data.unstack(level=0)
+    # Downsample data
+    data = data.resample("M").sum()
+    # Stack data
+    data = data.stack(level=["region"]).reset_index()
+    # Melt into long format for plotting
+    data = data.melt(id_vars=["p2a", "region"], var_name="p10", value_name="accidents")
+    # Plot data
+    g = sns.relplot(data=data, x="p2a", y="accidents", hue="p10", col="region", kind="line",
+                    col_wrap=2, aspect=1.1)
+    # Limit axis x axis from 1.1.2016 to 1.1.2023 and format x axis ticks
+    g.set(xlim=(np.datetime64("2016-01-01"), np.datetime64("2023-01-01")))
+    g.set(xticks=pd.date_range("2016-01-01", "2023-01-01", freq="YS"))
+    g.set(xticklabels=pd.date_range("2016-01-01", "2023-01-01", freq="YS").strftime("%m/%y"))
+    # Adjust space
+    plt.subplots_adjust(wspace=0.1)
+    # Set labels
+    g.set_titles("Kraj: {col_name}")
+    g.set_axis_labels("Datum", "Počet nehod")
+    # Set legend title
+    g.legend.set_title("Zavinění")
 
     if fig_location is not None:
         plt.savefig(fig_location)
@@ -163,5 +244,5 @@ if __name__ == "__main__":
     raw_df = load_data("data/data.zip")
     clean_df = parse_data(raw_df, True)
     plot_state(clean_df, "01_state.png")
-    plot_alcohol(clean_df, "02_alcohol.png", True)
+    plot_alcohol(clean_df, "02_alcohol.png")
     plot_fault(clean_df, "03_fault.png")
